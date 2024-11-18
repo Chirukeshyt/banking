f0cf83e91511427a5f9fea78624a0210adbb142d
D.Chirukesh <chirukeshdevabhathini@gmail.com>
2024-11-18 15:05:50 +0530

    update

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,10 +1,26 @@
-import React from 'react'
-
+import HeaderBox from '@/components/HeaderBox'
+import TotalBalanceBox from '@/components/TotalBalanceBox'
 const Home= () => {
+  const loggenIn = { firstName:'Chirukesh'}
   return (
-    <div>
-      <h1>home</h1>
-    </div>
+    <section className='home'>
+      <div className='home-content'>
+          <header className='home-header'>
+            <HeaderBox 
+             type="greeting"
+             title="welcome"
+             user={loggenIn?.firstName || 'Guest'}
+             subtext="Access and manage your account and transactions efficiently"
+            />
+            <TotalBalanceBox
+            accounts={[]}
+            totalBanks={1}
+            totalCurrentBalance={1250.35}
+
+            />
+          </header>
+      </div>
+    </section>
   )
 }
 
